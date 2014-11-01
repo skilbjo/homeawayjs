dd3d8af90773c75af046ceb2823b68f43ec6f510
skilbjo <john.skilbeck@gmail.com>
2014-10-31 17:35:28 -0700

    trying to refactor for a 0 dependency cronjob since want to deploy on a windows machine and cron sucks on windows

--- homeaway.js
+++ homeaway.js
@@ -4,34 +4,37 @@ var
   , _         = require('underscore')
   , fs        = require('fs')
   , S         = require('string')
+  , cron      = require('cron').CronJob
   , dt        = new Date();
 
 $.config = {
   site: 'http://www.homeaway.com/'
   , addJQuery: false };
 
-async.series([
-  $.go('visit', '/search')
-  , function(done) {
-    $('span.totalCount').data('hitcount', function(totalListings) {
-      $('#ols_more_filters').data('count', function(totalPaidListings) {
-        fs.appendFile('results.csv'
-          , S({Year: dt.getFullYear()
-            , Month: dt.getMonth() + 1
-            , Day: dt.getDate()
-            , TotalListings: totalListings
-            , totalPaidListings: totalPaidListings
-          }).toCSV().s + '\n'
-          , function(err) { if (err) throw err; console.log(dt.getFullYear() + '\t' + 
-            (dt.getMonth() + 1) + '\t' + 
-            dt.getDate() + '\t' +
-            totalListings + '\t' + 
-            totalPaidListings + '\n'); }
-        );
-        done();
+new cron('* * * * *', function() {
+  async.series([
+    $.go('visit', '/search')
+    , function(done) {
+      $('span.totalCount').data('hitcount', function(totalListings) {
+        $('#ols_more_filters').data('count', function(totalPaidListings) {
+          fs.appendFile('results.csv'
+            , S({Year: dt.getFullYear()
+              , Month: dt.getMonth() + 1
+              , Day: dt.getDate()
+              , TotalListings: totalListings
+              , totalPaidListings: totalPaidListings
+            }).toCSV().s + '\n'
+            , function(err) { if (err) throw err; console.log(dt.getFullYear() + '\t' + 
+              (dt.getMonth() + 1) + '\t' + 
+              dt.getDate() + '\t' +
+              totalListings + '\t' + 
+              totalPaidListings + '\n'); }
+          );
+          done();
+        });
       });
-    });
-  }
-], function() {
-  $.close();
-});
+    }
+  ], function() {
+    $.close();
+  });
+}, null, true);
